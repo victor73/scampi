bf9a36b9d1713becf050982b644a78201bf0c230
Victor <victor73@gmail.com>
2013-02-25 21:00:55 -0500

    Updated modules.

--- examples/topic_processor.sh
+++ examples/topic_processor.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#     _   ___     __             ____    ___
-#    | | / (_)___/ /____  ____  / __/__ / (_)_ __
-#    | |/ / / __/ __/ _ \/ __/ / _// -_) / /\ \ /
-#    |___/_/\__/\__/\___/_/   /_/  \__/_/_//_\_\
-#
-#    Author:
-#    Victor Felix     <vfelix@som.umaryland.edu>
-#
-
 read DOC
 
 echo $DOC >> /tmp/topic_log.log
